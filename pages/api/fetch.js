cd6f4b6fb687cfb07e93fb92c25b5c49094585be
Adam Jackson <akjackson1@gmail.com>
2022-12-11 17:10:46 -0500

    Properly calculate miles, not km

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -1,41 +1,43 @@
 import { distanceBetween } from "../../helpers";
 
 export default async function handler(req, res) {
-	const { lat, lng, radius = 50 } = req.query;
-	const country = "US";
+  const { lat, lng, radius = 50 } = req.query;
+  const country = "US";
 
-	const response = await fetch(`https://api.ebird.org/v2/data/obs/${country}/recent/notable?detail=full&key=${process.env.EBIRD_KEY}`);
-	let reports = await response.json();
+  const response = await fetch(
+    `https://api.ebird.org/v2/data/obs/${country}/recent/notable?detail=full&key=${process.env.EBIRD_KEY}`
+  );
+  let reports = await response.json();
 
-	if (!reports?.length) {
-		res.status(200).json([]);
-	}
-	
-	reports = reports
-		//Remove duplicates. For unknown reasons, eBird sometimes returns duplicates
-		.filter((value, index, array) => array.findIndex(searchItem => (searchItem.obsId === value.obsId)) === index)
-		.map(item => {
-			const distance = parseFloat(distanceBetween(lat, lng, item.lat, item.lng).toFixed(2));
-			return { ...item, distance };
-		})
-		.filter(({distance, comName}) => distance <= parseInt(radius) && ! comName.includes("(hybrid)"))
-		.map(item => ({...item, distance: parseInt(item.distance)}));
+  if (!reports?.length) {
+    res.status(200).json([]);
+  }
 
-	const reportsBySpecies = {};
+  reports = reports
+    //Remove duplicates. For unknown reasons, eBird sometimes returns duplicates
+    .filter((value, index, array) => array.findIndex((searchItem) => searchItem.obsId === value.obsId) === index)
+    .map((item) => {
+      const distance = parseFloat(distanceBetween(lat, lng, item.lat, item.lng, false).toFixed(2));
+      return { ...item, distance };
+    })
+    .filter(({ distance, comName }) => distance <= parseInt(radius) && !comName.includes("(hybrid)"))
+    .map((item) => ({ ...item, distance: parseInt(item.distance) }));
 
-	reports.forEach(item => {
-		if (!reportsBySpecies[item.speciesCode]) {
-			reportsBySpecies[item.speciesCode] = {
-				name: item.comName,
-				sciName: item.sciName,
-				code: item.speciesCode,
-				reports: [],
-			};
-		}
-		reportsBySpecies[item.speciesCode].reports.push(item);
-	});
+  const reportsBySpecies = {};
 
-	const species = Object.entries(reportsBySpecies).map(([key, value]) => value);
+  reports.forEach((item) => {
+    if (!reportsBySpecies[item.speciesCode]) {
+      reportsBySpecies[item.speciesCode] = {
+        name: item.comName,
+        sciName: item.sciName,
+        code: item.speciesCode,
+        reports: [],
+      };
+    }
+    reportsBySpecies[item.speciesCode].reports.push(item);
+  });
 
-	res.status(200).json([...species]);
+  const species = Object.entries(reportsBySpecies).map(([key, value]) => value);
+
+  res.status(200).json([...species]);
 }
