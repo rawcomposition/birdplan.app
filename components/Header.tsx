65d47a7b75efe0a499c3623076bd8a166d512346
Adam Jackson <akjackson1@gmail.com>
2023-03-17 13:38:20 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header({ title, parent }: Props) {
     <header className="bg-slate-900 h-[60px] shrink-0 flex items-center">
       <div className="w-80 flex items-center">
         <img src="/icon.png" className="w-[50px] mx-4" width="50" height="50" />
-        <h1 className="text-center text-[#757c8c] font-logo text-2xl">toucan plan</h1>
+        <h1 className="text-center text-[#757c8c] font-logo text-2xl">bird planner</h1>
       </div>
       <div className="mr-auto">
         {title && (
