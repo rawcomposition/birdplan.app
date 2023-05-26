34dadf0539deb05361c43f7ad9996077e2859da5
Adam Jackson <akjackson1@gmail.com>
2023-05-26 06:44:59 -0400

    Add View on Google Maps button

--- components/DirectionsButton.tsx
+++ components/DirectionsButton.tsx
@@ -28,48 +28,50 @@ export default function DirectionsButton({ lat, lng, hotspotId, markerId }: Prop
         <Directions className="mr-1 -mt-[3px] text-[#c2410d]" /> Directions
       </Button>
       <SlideOver open={open} onClose={() => setOpen(false)}>
-        <h3 className="font-bold mb-1 -mt-1.5 text-sm">Directions from...</h3>
-        <ul className="flex flex-col pl-2">
-          <li>
-            <Link
-              href={`https://www.google.com/maps/dir/?api=1&origin=Current+Location&destination=${lat},${lng}`}
-              className="flex items-center gap-2 text-sm cursor-pointer pb-2 pt-1.5"
-              target="_blank"
-            >
-              <span className="w-2.5 h-2.5 bg-sky-600 rounded-full ring-4 ring-sky-600/20 ml-2.5 mr-2" />
-              Current Location
-            </Link>
-          </li>
-          {filteredMarkers.map((marker) => (
-            <li key={marker.id}>
-              <Link
-                href={`https://www.google.com/maps/dir/?api=1&origin=${marker.lat},${marker.lng}&destination=${lat},${lng}`}
-                className="flex items-center gap-2 text-sm cursor-pointer"
-                target="_blank"
-              >
-                <MarkerWithIcon
-                  showStroke={false}
-                  offset={false}
-                  icon={marker.icon}
-                  className="inline-block ml-1 scale-75"
-                />
-                <span className="truncate">{marker.name}</span>
-              </Link>
-            </li>
-          ))}
-          {filteredHotspots.map((hotspot) => (
-            <li key={hotspot.id}>
-              <Link
-                href={`https://www.google.com/maps/dir/?api=1&origin=${hotspot.lat},${hotspot.lng}&destination=${lat},${lng}`}
-                className="flex items-center gap-2 text-sm cursor-pointer py-0.5"
-                target="_blank"
-              >
-                <Marker className="h-8 w-8 scale-75 shrink-0" color={getMarkerColor(hotspot.species || 0)} />
-                <span className="truncate">{hotspot.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <Link
+          href={`https://www.google.com/maps/search/?api=1&query=${lat},${lng}`}
+          className="flex items-center gap-2 text-sm cursor-pointer pb-2 mb-2.5"
+          target="_blank"
+        >
+          <Directions className="text-lg ml-3 mr-1.5 text-green-700" />
+          View on Google Maps
+        </Link>
+        {!!filteredHotspots?.length && !!filteredMarkers?.length && (
+          <>
+            <h3 className="font-bold mb-1 -mt-1.5 text-sm">Directions from...</h3>
+            <ul className="flex flex-col pl-2">
+              {filteredMarkers.map((marker) => (
+                <li key={marker.id}>
+                  <Link
+                    href={`https://www.google.com/maps/dir/?api=1&origin=${marker.lat},${marker.lng}&destination=${lat},${lng}`}
+                    className="flex items-center gap-2 text-sm cursor-pointer"
+                    target="_blank"
+                  >
+                    <MarkerWithIcon
+                      showStroke={false}
+                      offset={false}
+                      icon={marker.icon}
+                      className="inline-block ml-1 scale-75"
+                    />
+                    <span className="truncate">{marker.name}</span>
+                  </Link>
+                </li>
+              ))}
+              {filteredHotspots.map((hotspot) => (
+                <li key={hotspot.id}>
+                  <Link
+                    href={`https://www.google.com/maps/dir/?api=1&origin=${hotspot.lat},${hotspot.lng}&destination=${lat},${lng}`}
+                    className="flex items-center gap-2 text-sm cursor-pointer py-0.5"
+                    target="_blank"
+                  >
+                    <Marker className="h-8 w-8 scale-75 shrink-0" color={getMarkerColor(hotspot.species || 0)} />
+                    <span className="truncate">{hotspot.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </SlideOver>
     </>
   );
