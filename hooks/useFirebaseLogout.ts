611a6b62d7947ac7590389dfea0e975e59deae98
Adam Jackson <akjackson1@gmail.com>
2023-07-03 17:51:33 -0400

    Close sidebar on logout

--- hooks/useFirebaseLogout.ts
+++ hooks/useFirebaseLogout.ts
@@ -3,10 +3,12 @@ import { auth } from "lib/firebase";
 import { signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 import { useProfile } from "providers/profile";
+import { useUI } from "providers/ui";
 
 export default function useFirebaseLogout() {
   const [loading, setLoading] = React.useState(false);
   const router = useRouter();
+  const { closeSidebar } = useUI();
   const { reset: resetProfile } = useProfile();
 
   const logout = async () => {
@@ -15,6 +17,7 @@ export default function useFirebaseLogout() {
       await signOut(auth);
       resetProfile();
       localStorage.clear();
+      closeSidebar();
       router.push("/");
     } catch (error) {
       alert("Error logging out");
