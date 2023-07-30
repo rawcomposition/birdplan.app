c67e9375df818c6de76f875f5c3846691128cd53
Adam Jackson <akjackson1@gmail.com>
2023-07-30 15:49:54 -0400

    Show recentNeeds labels

--- components/RecentSpeciesSidebarBlock.tsx
+++ components/RecentSpeciesSidebarBlock.tsx
@@ -3,6 +3,8 @@ import Expand from "components/Expand";
 import SpeciesRow from "components/SpeciesRow";
 import { RecentSpecies } from "lib/types";
 import Search from "components/Search";
+import dayjs from "dayjs";
+import { useTrip } from "providers/trip";
 
 type Props = {
   recentSpecies: RecentSpecies[];
@@ -10,16 +12,61 @@ type Props = {
 
 export default function RecentSpeciesSidebarBlock({ recentSpecies }: Props) {
   const [search, setSearch] = React.useState("");
+  const { trip } = useTrip();
+  const tz = trip?.timezone;
 
   const filteredSpecies = recentSpecies.filter((it) => it.name.toLowerCase().includes(search.toLowerCase()));
+
+  const today = dayjs().tz(tz).format("YYYY-MM-DD");
+  const yesterday = dayjs().tz(tz).subtract(1, "day").format("YYYY-MM-DD");
+  let todayUsed = false;
+  let yesterdayUsed = false;
+  let gtSevenDaysAgoUsed = false;
+  let gtFifteenDaysAgoUsed = false;
 
   return (
     <Expand heading="Recent Needs" count={recentSpecies.length} focusInput>
       {recentSpecies.length > 0 && <Search value={search} onChange={setSearch} />}
       <ul className="divide-y divide-gray-800">
-        {filteredSpecies.map(({ code, name }) => (
-          <SpeciesRow key={code} name={name} code={code} />
-        ))}
+        {filteredSpecies.map(({ code, name, date }) => {
+          const dateFormatted = dayjs(date).tz(tz).format("YYYY-MM-DD");
+
+          let showToday = false;
+          let showYesterday = false;
+          let showGtSevenDaysAgo = false;
+          let showGtFifteenDaysAgo = false;
+
+          if (dateFormatted === today) {
+            if (!todayUsed) {
+              showToday = true;
+              todayUsed = true;
+            }
+          } else if (dateFormatted === yesterday) {
+            if (!yesterdayUsed) {
+              yesterdayUsed = true;
+              showYesterday = true;
+            }
+          } else if (dayjs(date).tz(tz).isAfter(dayjs().tz(tz).subtract(7, "day"))) {
+            if (!gtSevenDaysAgoUsed) {
+              gtSevenDaysAgoUsed = true;
+              showGtSevenDaysAgo = true;
+            }
+          } else if (dayjs(date).tz(tz).isAfter(dayjs().tz(tz).subtract(15, "day"))) {
+            if (!gtFifteenDaysAgoUsed) {
+              gtFifteenDaysAgoUsed = true;
+              showGtFifteenDaysAgo = true;
+            }
+          }
+          return (
+            <>
+              {showToday && <li className="text-xs text-gray-500 py-1 pt-4">Today</li>}
+              {showYesterday && <li className="text-xs text-gray-500 py-1 pt-4">Yesterday</li>}
+              {showGtSevenDaysAgo && <li className="text-xs text-gray-500 py-1 pt-4">More than 7 days ago</li>}
+              {showGtFifteenDaysAgo && <li className="text-xs text-gray-500 py-1 pt-4">More than 15 days ago</li>}
+              <SpeciesRow key={code} name={name} code={code} />
+            </>
+          );
+        })}
       </ul>
     </Expand>
   );
