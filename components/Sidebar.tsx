cc76ec0ad13feb611f2b8bbd28e82d281b264e81
Adam Jackson <akjackson1@gmail.com>
2023-11-01 08:33:13 -0400

    Hide sidebar when navigating

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx";
 import Account from "components/Account";
 import { useUI } from "providers/ui";
+import { useRouter } from "next/router";
+import React from "react";
 
 type Props = {
   className?: string;
@@ -12,14 +14,19 @@ type Props = {
 };
 
 export default function Sidebar({ children, className, noPadding, fullWidth, noAnimation, noAccount }: Props) {
-  const { sidebarOpen } = useUI();
+  const { sidebarOpen, closeSidebar } = useUI();
+  const { pathname } = useRouter();
+
+  React.useEffect(() => {
+    closeSidebar();
+  }, [pathname]);
 
   return (
     <aside
       className={clsx(
         "flex flex-shrink-0 flex-col h-[calc(100%_-_60px)] md:h-full md:ml-0 bg-[#1e263a] absolute md:relative shadow-2xl md:shadow-none z-10 overflow-y-auto dark-scrollbar",
         noPadding ? "" : "p-6",
-        !sidebarOpen && "-ml-[450px]",
+        !sidebarOpen && "-ml-[600px]",
         fullWidth ? "w-full sm:w-80" : "w-80",
         noAnimation ? "" : "transition-all",
         className
