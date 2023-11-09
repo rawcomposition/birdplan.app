16dca256447cb45be8a98edc20a55b334c250f8e
Adam Jackson <akjackson1@gmail.com>
2023-11-09 09:04:06 -0500

    Revert "Update useFirebaseLogin.ts"
    This reverts commit cab75a1e662d16062b6327ba92323b864ee34d8d.

--- hooks/useFirebaseLogin.ts
+++ hooks/useFirebaseLogin.ts
@@ -1,6 +1,6 @@
 import React from "react";
 import { auth } from "lib/firebase";
-import { getAuth, signInWithPopup, GoogleAuthProvider, indexedDBLocalPersistence, setPersistence } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, indexedDBLocalPersistence, setPersistence } from "firebase/auth";
 import toast from "react-hot-toast";
 import { useRouter } from "next/router";
 
@@ -10,20 +10,17 @@ export default function useFirebaseLogin() {
 
   const login = async () => {
     setLoading(true);
-    const auth = getAuth();
-    setPersistence(auth, indexedDBLocalPersistence)
-      .then(() => {
-        const provider = new GoogleAuthProvider();
-        // In memory persistence will be applied to the signed in Google user
-        // even though the persistence was set to 'none' and a page redirect
-        // occurred.
-        return signInWithPopup(auth, provider);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
+    await setPersistence(auth, indexedDBLocalPersistence);
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+      router.push("/trips");
+    } catch (error) {
+      toast.error("Login failed");
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return { login, loading };
