8c3128ffd1c6e8fe62625bd69dd279ec44c147ac
Adam Jackson <akjackson1@gmail.com>
2023-11-20 08:50:09 -0500

    Scroll itinerary body

--- components/ItineraryBuilder.tsx
+++ components/ItineraryBuilder.tsx
@@ -44,144 +44,147 @@ export default function ItineraryBuilder() {
   };
 
   return (
-    <div className="mt-8 max-w-2xl w-full mx-auto p-4 md:p-0">
-      <div className="mb-12">
-        <div className="flex justify-between items-center">
-          <h1 className="text-3xl font-bold text-gray-700">Trip Itinerary</h1>
-          {canEdit && (
-            <Button
-              size="smPill"
-              color="pillOutlineGray"
-              className="flex items-center gap-2 print:hidden"
-              onClick={() => setEditing((prev) => !prev)}
-            >
-              {isEditing ? <CheckIcon className="w-4 h-4" /> : <Pencil className="w-4 h-4" />}
-              <span>{isEditing ? "Done" : "Edit"}</span>
-            </Button>
-          )}
-        </div>
-        {canEdit && !!trip?.startDate && !editingStartDate && (
-          <button
-            type="button"
-            onClick={() => setEditingStartDate(true)}
-            className="text-[14px] text-gray-600 hover:text-gray-700 block mt-2 hover:underline"
-          >
-            Edit start date
-          </button>
-        )}
-      </div>
-
-      {canEdit && (!trip?.startDate || editingStartDate) && (
-        <div className="pt-4 p-5 bg-white rounded-lg shadow mb-8">
-          <h2 className="text-xl font-bold text-gray-700 mb-4">Choose start date</h2>
-          <form className="flex gap-2" onSubmit={submitStartDate}>
-            <Input name="date" type="date" defaultValue={trip?.startDate} />
-            <Button type="submit" color="primary">
-              Set
-            </Button>
-          </form>
-        </div>
-      )}
-      {!canEdit && !trip?.startDate && (
-        <div className="pt-4 p-5 bg-white rounded-lg shadow mb-8">No itinerary has been set for this trip yet.</div>
-      )}
-      {trip?.itinerary.map(({ id, locations }, i) => {
-        const date = dayjs(trip.startDate).add(i, "day").format("dddd, MMMM D");
-        return (
-          <div key={id} className="mb-8">
-            <div className="mb-3">
-              <h1 className="text-xl font-bold text-gray-700">Day {i + 1}</h1>
-              <span className="text-gray-500 text-sm">{date}</span>
-            </div>
-            {!!locations?.length && (
-              <ul className="flex flex-col">
-                {locations?.map(({ locationId, type }, index) => {
-                  const location =
-                    trip?.hotspots?.find((h) => h.id === locationId) || trip?.markers?.find((m) => m.id === locationId);
-                  return (
-                    <React.Fragment key={locationId}>
-                      {index !== 0 && (
-                        <li>
-                          <TravelTime isEditing={isEditing} dayId={id} locationId={locationId} />
-                        </li>
-                      )}
-                      <li
-                        key={locationId}
-                        className="flex items-center gap-2 text-sm text-gray-700 group relative p-3 bg-white rounded-lg shadow"
-                        onClick={
-                          !isEditing
-                            ? () =>
-                                type === "hotspot"
-                                  ? open("hotspot", { hotspot: location })
-                                  : open("viewMarker", { marker: location })
-                            : undefined
-                        }
-                        aria-label="View location"
-                        role="button"
-                      >
-                        <MarkerWithIcon
-                          showStroke={false}
-                          icon={(location as any)?.icon || "hotspot"}
-                          className="inline-block scale-[.85] flex-shrink-0 print:hidden"
-                        />
-                        <span className="truncate font-medium">{location?.name || "Unknown Location"}</span>
-                        {isEditing && (
-                          <div className="flex items-center gap-1.5 ml-auto">
-                            {index !== locations.length - 1 && (
-                              <button
-                                type="button"
-                                onClick={() => moveItineraryDayLocation(id, locationId, "down")}
-                                className="text-[16px] p-1 text-gray-600 opacity-0 group-hover:opacity-100 transition-opacity -mt-px"
-                              >
-                                <AngleDownBold />
-                              </button>
-                            )}
-                            {index !== 0 && (
-                              <button
-                                type="button"
-                                onClick={() => moveItineraryDayLocation(id, locationId, "up")}
-                                className="text-[16px] p-1 -mt-1 text-gray-600 opacity-0 group-hover:opacity-100 transition-opacity"
-                              >
-                                <AngleDownBold className="rotate-180" />
-                              </button>
-                            )}
-                            <button
-                              type="button"
-                              onClick={() => removeItineraryDayLocation(id, locationId)}
-                              className="text-[16px] p-1 -mt-1 text-gray-600 opacity-0 group-hover:opacity-100 transition-opacity"
-                            >
-                              <XMarkBold />
-                            </button>
-                          </div>
-                        )}
-                      </li>
-                    </React.Fragment>
-                  );
-                })}
-              </ul>
-            )}
-            {isEditing && (
-              <div className="flex justify-between items-center gap-2 mt-3">
-                <Button size="xs" color="gray" onClick={() => open("addItineraryLocation", { dayId: id })}>
-                  + Add Location
-                </Button>
-                <button
-                  type="button"
-                  onClick={() => handleRemoveDay(id)}
-                  className="text-[12px] py-0.5 px-1.5 text-red-700"
-                >
-                  Remove day
-                </button>
-              </div>
+    <div className="h-full overflow-auto">
+      <div className="mt-8 max-w-2xl w-full mx-auto p-4 md:p-0">
+        <div className="mb-12">
+          <div className="flex justify-between items-center">
+            <h1 className="text-3xl font-bold text-gray-700">Trip Itinerary</h1>
+            {canEdit && (
+              <Button
+                size="smPill"
+                color="pillOutlineGray"
+                className="flex items-center gap-2 print:hidden"
+                onClick={() => setEditing((prev) => !prev)}
+              >
+                {isEditing ? <CheckIcon className="w-4 h-4" /> : <Pencil className="w-4 h-4" />}
+                <span>{isEditing ? "Done" : "Edit"}</span>
+              </Button>
             )}
           </div>
-        );
-      })}
-      {isEditing && (
-        <Button color="primary" onClick={appendItineraryDay} className="mb-8">
-          Add Day
-        </Button>
-      )}
+          {canEdit && !!trip?.startDate && !editingStartDate && (
+            <button
+              type="button"
+              onClick={() => setEditingStartDate(true)}
+              className="text-[14px] text-gray-600 hover:text-gray-700 block mt-2 hover:underline"
+            >
+              Edit start date
+            </button>
+          )}
+        </div>
+
+        {canEdit && (!trip?.startDate || editingStartDate) && (
+          <div className="pt-4 p-5 bg-white rounded-lg shadow mb-8">
+            <h2 className="text-xl font-bold text-gray-700 mb-4">Choose start date</h2>
+            <form className="flex gap-2" onSubmit={submitStartDate}>
+              <Input name="date" type="date" defaultValue={trip?.startDate} />
+              <Button type="submit" color="primary">
+                Set
+              </Button>
+            </form>
+          </div>
+        )}
+        {!canEdit && !trip?.startDate && (
+          <div className="pt-4 p-5 bg-white rounded-lg shadow mb-8">No itinerary has been set for this trip yet.</div>
+        )}
+        {trip?.itinerary.map(({ id, locations }, i) => {
+          const date = dayjs(trip.startDate).add(i, "day").format("dddd, MMMM D");
+          return (
+            <div key={id} className="mb-8">
+              <div className="mb-3">
+                <h1 className="text-xl font-bold text-gray-700">Day {i + 1}</h1>
+                <span className="text-gray-500 text-sm">{date}</span>
+              </div>
+              {!!locations?.length && (
+                <ul className="flex flex-col">
+                  {locations?.map(({ locationId, type }, index) => {
+                    const location =
+                      trip?.hotspots?.find((h) => h.id === locationId) ||
+                      trip?.markers?.find((m) => m.id === locationId);
+                    return (
+                      <React.Fragment key={locationId}>
+                        {index !== 0 && (
+                          <li>
+                            <TravelTime isEditing={isEditing} dayId={id} locationId={locationId} />
+                          </li>
+                        )}
+                        <li
+                          key={locationId}
+                          className="flex items-center gap-2 text-sm text-gray-700 group relative p-3 bg-white rounded-lg shadow"
+                          onClick={
+                            !isEditing
+                              ? () =>
+                                  type === "hotspot"
+                                    ? open("hotspot", { hotspot: location })
+                                    : open("viewMarker", { marker: location })
+                              : undefined
+                          }
+                          aria-label="View location"
+                          role="button"
+                        >
+                          <MarkerWithIcon
+                            showStroke={false}
+                            icon={(location as any)?.icon || "hotspot"}
+                            className="inline-block scale-[.85] flex-shrink-0 print:hidden"
+                          />
+                          <span className="truncate font-medium">{location?.name || "Unknown Location"}</span>
+                          {isEditing && (
+                            <div className="flex items-center gap-1.5 ml-auto">
+                              {index !== locations.length - 1 && (
+                                <button
+                                  type="button"
+                                  onClick={() => moveItineraryDayLocation(id, locationId, "down")}
+                                  className="text-[16px] p-1 text-gray-600 opacity-0 group-hover:opacity-100 transition-opacity -mt-px"
+                                >
+                                  <AngleDownBold />
+                                </button>
+                              )}
+                              {index !== 0 && (
+                                <button
+                                  type="button"
+                                  onClick={() => moveItineraryDayLocation(id, locationId, "up")}
+                                  className="text-[16px] p-1 -mt-1 text-gray-600 opacity-0 group-hover:opacity-100 transition-opacity"
+                                >
+                                  <AngleDownBold className="rotate-180" />
+                                </button>
+                              )}
+                              <button
+                                type="button"
+                                onClick={() => removeItineraryDayLocation(id, locationId)}
+                                className="text-[16px] p-1 -mt-1 text-gray-600 opacity-0 group-hover:opacity-100 transition-opacity"
+                              >
+                                <XMarkBold />
+                              </button>
+                            </div>
+                          )}
+                        </li>
+                      </React.Fragment>
+                    );
+                  })}
+                </ul>
+              )}
+              {isEditing && (
+                <div className="flex justify-between items-center gap-2 mt-3">
+                  <Button size="xs" color="gray" onClick={() => open("addItineraryLocation", { dayId: id })}>
+                    + Add Location
+                  </Button>
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveDay(id)}
+                    className="text-[12px] py-0.5 px-1.5 text-red-700"
+                  >
+                    Remove day
+                  </button>
+                </div>
+              )}
+            </div>
+          );
+        })}
+        {isEditing && (
+          <Button color="primary" onClick={appendItineraryDay} className="mb-8">
+            Add Day
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
