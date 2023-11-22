357f575528dfe542796fd63bf2427661400e37b5
Adam Jackson <akjackson1@gmail.com>
2023-11-22 09:04:37 -0500

    Open in Organic Maps deep link

--- components/DirectionsButton.tsx
+++ components/DirectionsButton.tsx
@@ -7,6 +7,7 @@ import MarkerWithIcon from "components/MarkerWithIcon";
 import GoogleMaps from "icons/GoogleMaps";
 import Link from "next/link";
 import { MarkerIcon } from "lib/types";
+import OrganicMaps from "icons/OrganicMaps";
 
 type Props = {
   lat: number;
@@ -21,6 +22,7 @@ export default function DirectionsButton({ lat, lng, hotspotId, markerId }: Prop
 
   const filteredHotspots = trip?.hotspots.filter((it) => it.id !== hotspotId) || [];
   const filteredMarkers = trip?.markers.filter((it) => it.id !== markerId) || [];
+  const hotspot = trip?.hotspots.find((it) => it.id === hotspotId);
 
   const isDirect = filteredHotspots.length === 0 && filteredMarkers.length === 0;
 
@@ -36,14 +38,22 @@ export default function DirectionsButton({ lat, lng, hotspotId, markerId }: Prop
         <Directions className="mr-1 -mt-[3px] text-[#c2410d]" /> Directions
       </Button>
       <SlideOver open={open} onClose={() => setOpen(false)}>
-        <Link
+        <a
           href={`https://www.google.com/maps/search/?api=1&query=${lat},${lng}`}
           className="flex items-center gap-2 text-sm cursor-pointer pb-2 mb-2.5 text-gray-700"
           target="_blank"
         >
           <GoogleMaps className="text-lg ml-3.5 mr-1.5 flex-shrink-0" />
-          View on Google Maps
-        </Link>
+          View in Google Maps
+        </a>
+        <a
+          href={`om://map?v=1&ll=${lat},${lng}&n=${hotspot?.name || ""}`}
+          className="flex items-center gap-2 text-sm cursor-pointer pb-2 mb-2.5 text-gray-700 sm:hidden"
+          target="_blank"
+        >
+          <OrganicMaps className="text-lg ml-3.5 mr-1.5 flex-shrink-0" />
+          View in Organic Maps
+        </a>
         {(!!filteredHotspots?.length || !!filteredMarkers?.length) && (
           <>
             <h3 className="font-bold mb-1.5 -mt-1.5 text-sm">Directions from...</h3>
