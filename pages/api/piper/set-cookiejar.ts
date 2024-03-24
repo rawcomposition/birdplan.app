74dac50df53dc609b7f89f94c9b71bde39050edf
Adam Jackson <akjackson1@gmail.com>
2024-03-24 13:57:09 -0400

    Create set-cookiejar.ts

---
+++ pages/api/piper/set-cookiejar.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "lib/firebaseAdmin";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  try {
+    const key = req.query.key;
+    const body = req.body;
+
+    if (!key || key !== process.env.PIPER_KEY) throw new Error("Invalid key");
+    if (!body) throw new Error("Missing body");
+
+    await db.collection("vault").doc("ebird-tools").set({ cookiejar: body });
+
+    res.status(200).send("Success");
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+}
