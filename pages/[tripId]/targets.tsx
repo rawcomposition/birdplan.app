c1a15a7d3e846d4b5af497f41c8e9044d2f233e0
Adam Jackson <akjackson1@gmail.com>
2024-06-02 17:27:15 -0400

    Improve target map display

--- pages/[tripId]/targets.tsx
+++ pages/[tripId]/targets.tsx
@@ -77,106 +77,114 @@ export default function TripTargets() {
 
       <Header title={trip?.name || ""} parent={{ title: "Trips", href: user?.uid ? "/trips" : "/" }} />
       <TripNav active="targets" />
-      <main className="flex h-[calc(100%-60px-52px)]">
+      <main className="flex h-[calc(100%-60px-52px)] relative">
         <ErrorBoundary>
-          <div className="h-full grow flex sm:relative flex-col w-full">
-            <div className="h-full w-full mx-auto max-w-6xl">
-              <ProfileSelect value={uid} onChange={setUid} />
-              {!!targetSpecies?.length && (
-                <div className="flex items-center gap-2 my-2 sm:my-4 px-2 sm:px-0">
-                  <Input
-                    type="search"
-                    value={search}
-                    onChange={(e: any) => setSearch(e.target.value)}
-                    placeholder="Search species"
-                    className="max-w-xs"
-                  />
-                  <label className="flex items-center gap-2 py-2 px-3 text-gray-600 text-sm">
-                    <input
-                      type="checkbox"
-                      checked={showStarred}
-                      onChange={() => setShowStarred(!showStarred)}
-                      className="form-checkbox text-sky-600"
+          <div className="h-full overflow-auto w-full">
+            <div className="h-full grow flex sm:relative flex-col w-full">
+              <div className="h-full w-full mx-auto max-w-6xl">
+                <ProfileSelect value={uid} onChange={setUid} />
+                {!!targetSpecies?.length && (
+                  <div className="flex items-center gap-2 my-2 sm:my-4 px-2 sm:px-0">
+                    <Input
+                      type="search"
+                      value={search}
+                      onChange={(e: any) => setSearch(e.target.value)}
+                      placeholder="Search species"
+                      className="max-w-xs"
                     />
-                    <span className="text-gray-600 text-sm">Starred</span>
-                  </label>
-                </div>
-              )}
-              {!!targets?.N && !truncatedTargets?.length && (
-                <div className="sm:bg-white sm:rounded-lg sm:shadow p-4 text-center mt-4">
-                  <h3 className="text-lg font-medium mb-2 text-gray-700">No targets found</h3>
-                  <p className="text-gray-500 text-sm">
-                    {truncatedTargets?.length === truncatedTargets?.length
-                      ? "It looks like you have already seen all the species in this region."
-                      : "No targets found for your search."}
-                  </p>
-                </div>
-              )}
-              {!targets?.N && !truncatedTargets?.length && (
-                <div className="sm:bg-white sm:rounded-lg sm:shadow p-4 text-center mt-4 space-y-2">
-                  {canEdit ? (
-                    <h3 className="text-lg font-medium text-gray-700">You haven&apos;t imported your targets yet</h3>
-                  ) : (
-                    <h3 className="text-lg font-medium text-gray-700">No targets have been imported yet</h3>
-                  )}
-                  {canEdit && (
-                    <p>
-                      <Button href={`/${trip?.id}/import-targets?redirect=targets&back=true`} color="primary" size="sm">
-                        Import Targets
-                      </Button>
+                    <label className="flex items-center gap-2 py-2 px-3 text-gray-600 text-sm">
+                      <input
+                        type="checkbox"
+                        checked={showStarred}
+                        onChange={() => setShowStarred(!showStarred)}
+                        className="form-checkbox text-sky-600"
+                      />
+                      <span className="text-gray-600 text-sm">Starred</span>
+                    </label>
+                  </div>
+                )}
+                {!!targets?.N && !truncatedTargets?.length && (
+                  <div className="sm:bg-white sm:rounded-lg sm:shadow p-4 text-center mt-4">
+                    <h3 className="text-lg font-medium mb-2 text-gray-700">No targets found</h3>
+                    <p className="text-gray-500 text-sm">
+                      {truncatedTargets?.length === truncatedTargets?.length
+                        ? "It looks like you have already seen all the species in this region."
+                        : "No targets found for your search."}
                     </p>
+                  </div>
+                )}
+                {!targets?.N && !truncatedTargets?.length && (
+                  <div className="sm:bg-white sm:rounded-lg sm:shadow p-4 text-center mt-4 space-y-2">
+                    {canEdit ? (
+                      <h3 className="text-lg font-medium text-gray-700">You haven&apos;t imported your targets yet</h3>
+                    ) : (
+                      <h3 className="text-lg font-medium text-gray-700">No targets have been imported yet</h3>
+                    )}
+                    {canEdit && (
+                      <p>
+                        <Button
+                          href={`/${trip?.id}/import-targets?redirect=targets&back=true`}
+                          color="primary"
+                          size="sm"
+                        >
+                          Import Targets
+                        </Button>
+                      </p>
+                    )}
+                  </div>
+                )}
+                {!!truncatedTargets?.length && (
+                  <table className="divide-y w-full">
+                    <thead className="hidden sm:table-header-group">
+                      <tr>
+                        <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1 px-4 w-0">#</th>
+                        <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1 w-[4.3rem] lg:w-20">
+                          Image
+                        </th>
+                        <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1">Species</th>
+                        <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1 w-0 hidden md:table-cell">
+                          Notes
+                        </th>
+                        <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1 md:w-12 lg:w-20">
+                          %
+                        </th>
+                        <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1">Last seen</th>
+                        <th className="w-0" />
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y">
+                      {truncatedTargets?.map((it, index) => (
+                        <TargetRow key={it.code} {...it} index={index} />
+                      ))}
+                    </tbody>
+                  </table>
+                )}
+
+                <div className="my-4 text-center pb-4">
+                  {filteredTargets?.length > showCount && (
+                    <button type="button" className="text-sky-600 font-bold text-sm" onClick={() => setPage(page + 1)}>
+                      Show {Math.min(filteredTargets.length - showCount, 50)} more
+                    </button>
                   )}
                 </div>
-              )}
-              {!!truncatedTargets?.length && (
-                <table className="divide-y w-full">
-                  <thead className="hidden sm:table-header-group">
-                    <tr>
-                      <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1 px-4 w-0">#</th>
-                      <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1 w-[4.3rem] lg:w-20">
-                        Image
-                      </th>
-                      <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1">Species</th>
-                      <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1 w-0 hidden md:table-cell">
-                        Notes
-                      </th>
-                      <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1 md:w-12 lg:w-20">%</th>
-                      <th className="text-left text-gray-500 font-normal uppercase text-xs pb-1">Last seen</th>
-                      <th className="w-0" />
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y">
-                    {truncatedTargets?.map((it, index) => (
-                      <TargetRow key={it.code} {...it} index={index} />
-                    ))}
-                  </tbody>
-                </table>
-              )}
-
-              <div className="my-4 text-center pb-4">
-                {filteredTargets?.length > showCount && (
-                  <button type="button" className="text-sky-600 font-bold text-sm" onClick={() => setPage(page + 1)}>
-                    Show {Math.min(filteredTargets.length - showCount, 50)} more
-                  </button>
+              </div>
+            </div>
+          </div>
+          {selectedSpecies && (
+            <div className="absolute inset-0 z-10">
+              {selectedSpecies && <SpeciesCard name={selectedSpecies.name} code={selectedSpecies.code} />}
+              <div className="w-full h-full relative">
+                {trip?.bounds && (
+                  <MapBox
+                    key={trip.id}
+                    onHotspotClick={obsClick}
+                    obsLayer={selectedSpecies && obsLayer}
+                    bounds={trip.bounds}
+                  />
                 )}
               </div>
             </div>
-            {selectedSpecies && (
-              <div className="fixed inset-0 z-20">
-                {selectedSpecies && <SpeciesCard name={selectedSpecies.name} code={selectedSpecies.code} />}
-                <div className="w-full h-full relative">
-                  {trip?.bounds && (
-                    <MapBox
-                      key={trip.id}
-                      onHotspotClick={obsClick}
-                      obsLayer={selectedSpecies && obsLayer}
-                      bounds={trip.bounds}
-                    />
-                  )}
-                </div>
-              </div>
-            )}
-          </div>
+          )}
         </ErrorBoundary>
       </main>
     </div>
