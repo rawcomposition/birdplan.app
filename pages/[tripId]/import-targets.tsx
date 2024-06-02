df405170bc8407b08d599be34dd545a5c7afc071
Adam Jackson <akjackson1@gmail.com>
2024-06-02 17:07:37 -0400

    Restore cutoff option

--- pages/[tripId]/import-targets.tsx
+++ pages/[tripId]/import-targets.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import Header from "components/Header";
 import Head from "next/head";
 import { useTrip } from "providers/trip";
-import toast from "react-hot-toast";
+import Select from "components/ReactSelectStyled";
+import { Option } from "lib/types";
 import Button from "components/Button";
 import { useProfile } from "providers/profile";
 import LoginModal from "components/LoginModal";
@@ -13,8 +14,13 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import NotFound from "components/NotFound";
 import { useQuery } from "@tanstack/react-query";
+import clsx from "clsx";
+
+const cutoffs = ["5%", "2%", "1%", "0.8%", "0.5%", "0.2%", "0.1%", "0%"];
 
 export default function ImportTargets() {
+  const [cutoff, setCutoff] = React.useState<Option>({ value: "1%", label: "1%" });
+  const [isFinalizing, setIsFinalizing] = React.useState(false);
   const { is404, trip, setTargets } = useTrip();
   const { lifelist } = useProfile();
   const router = useRouter();
@@ -37,17 +43,17 @@ export default function ImportTargets() {
     refetchOnWindowFocus: false,
     staleTime: 0,
     cacheTime: 0,
-    enabled: false,
+    enabled: !!region,
   });
 
-  React.useEffect(() => {
-    if (data && tripId) {
-      setTargets({ ...data, tripId });
-      setTimeout(() => {
-        router.push(redirectUrl);
-      }, 1000);
-    }
-  }, [data, tripId]);
+  const handleSubmit = async () => {
+    if (!tripId || !data) return;
+    const filtered = data.items.filter(({ percent }) => percent >= Number(cutoff.value.replace("%", "")));
+    setIsFinalizing(true);
+    await setTargets({ ...data, tripId, items: filtered });
+    setIsFinalizing(false);
+    router.push(redirectUrl);
+  };
 
   if (is404) return <NotFound />;
 
@@ -66,29 +72,67 @@ export default function ImportTargets() {
         )}
         <div className="p-4 md:p-0 mt-8">
           <h1 className="text-3xl font-bold text-gray-700 mb-8">🎯 Import Targets</h1>
-          {(isLoading || isRefetching) && (
-            <div className="pt-4 p-5 bg-white rounded-lg shadow mb-8 flex flex-col items-center text-center gap-4">
-              <Icon name="loading" className="animate-spin text-4xl text-slate-500" />
-              <p className="text-md text-slate-600">Importing from eBird, this may take a minute...</p>
+          <div className="pt-4 p-5 bg-white rounded-lg shadow mb-8 flex flex-col gap-4">
+            <h3 className="text-lg font-medium mb-4 text-gray-700">1. Download from eBird</h3>
+            {(isLoading || isRefetching) && (
+              <div className="flex gap-2 items-center">
+                <Icon name="loading" className="animate-spin text-md text-slate-500" />
+                <p className="text-md text-slate-600">This may take a minute...</p>
+              </div>
+            )}
+            {!!error && !isRefetching && (
+              <div className="flex gap-2 items-center">
+                <Icon name="xMark" className="text-lg text-red-500" />
+                <p className="text-md text-slate-600">Error downloading from eBird.</p>
+                <button className="text-sky-600 font-medium" onClick={() => refetch()}>
+                  Retry
+                </button>
+              </div>
+            )}
+            {!isLoading && !isRefetching && !!data && (
+              <div className="flex gap-2 items-center">
+                <Icon name="check" className="text-lg text-green-500" />
+                <p className="text-md text-slate-600">Targets download!</p>
+              </div>
+            )}
+          </div>
+          <div
+            className={clsx(
+              "pt-4 p-5 bg-white rounded-lg shadow mb-8",
+              (!data || isRefetching || isLoading) && "opacity-60 pointer-events-none"
+            )}
+          >
+            <h3 className="text-lg font-medium mb-4 text-gray-700">2. Choose a cutoff</h3>
+            <label htmlFor="cutoff" className="text-sm text-gray-600 mb-2 block">
+              Ignore targets below
+            </label>
+            <div className="max-w-xs">
+              <Select
+                id="cutoff"
+                options={cutoffs.map((it) => ({ value: it, label: it }))}
+                value={cutoff}
+                onChange={setCutoff}
+                menuPortalTarget={typeof document !== "undefined" && document.body}
+              />
             </div>
-          )}
-          {!!error && !isRefetching && (
-            <div className="pt-4 p-5 bg-white rounded-lg shadow mb-8 flex flex-col items-center text-center gap-4">
-              <p className="text-md text-red-600">Error importing from eBird.</p>
-              <Button onClick={() => refetch()}>Retry</Button>
-            </div>
-          )}
-          {!isLoading && !isRefetching && !!data && (
-            <div className="pt-4 p-5 bg-white rounded-lg shadow mb-8 flex flex-col items-center text-center gap-2">
-              <Icon name="check" className="text-4xl text-slate-500" />
-              <p className="text-md text-slate-600">Targets imported!</p>
-            </div>
-          )}
+          </div>
 
           <div className="flex">
-            {!!data ? (
-              <Button href={redirectUrl} color="primary" className="inline-flex items-center ml-auto">
-                Continue
+            {!!cutoff && data && !isLoading && !isRefetching && !error ? (
+              <Button
+                onClick={handleSubmit}
+                color="primary"
+                className="inline-flex items-center ml-auto"
+                disabled={isFinalizing}
+              >
+                {isFinalizing ? (
+                  <>
+                    <Icon name="loading" className="animate-spin text-md text-white" />
+                    <span className="ml-2">Saving...</span>
+                  </>
+                ) : (
+                  "Continue"
+                )}
               </Button>
             ) : (
               <Button href={redirectUrl} color="gray" className="inline-flex items-center ml-auto">
