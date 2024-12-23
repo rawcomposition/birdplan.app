280ba60d9e297b142a62c6f6296dbccc7fab7f01
Adam Jackson <akjackson1@gmail.com>
2024-12-23 12:09:10 -0800

    Update set-cookiejar.ts

--- pages/api/piper/set-cookiejar.ts
+++ pages/api/piper/set-cookiejar.ts
@@ -1,21 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "lib/firebaseAdmin";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
     const key = req.query.key;
-    const body = req.body.toString(); // Convert to string to avoid JSON parsing
-    console.log("TYPEOF", typeof body);
-    console.log(body);
 
     if (!key || key !== process.env.PIPER_KEY) throw new Error("Invalid key");
-    if (!body) throw new Error("Missing body");
 
-    await db.collection("vault").doc("ebird-tools").set({ cookiejar: body });
+    let rawBody = "";
+    await new Promise<void>((resolve, reject) => {
+      req.on("data", (chunk) => {
+        rawBody += chunk.toString(); // Collect raw body
+      });
+      req.on("end", resolve);
+      req.on("error", reject);
+    });
+
+    if (!rawBody) throw new Error("Missing body");
+
+    await db.collection("vault").doc("ebird-tools").set({ cookiejar: rawBody });
 
     res.status(200).send("Success");
   } catch (error: any) {
-    console.log(error);
+    console.error(error);
     res.status(500).json({ error: error.message });
   }
 }
