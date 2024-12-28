52bb487a3e5b816f3e32bbdb9f74dd3dc2ff312d
Adam Jackson <akjackson1@gmail.com>
2024-12-27 16:36:38 -0800

    Fix infinite loop issue

--- hooks/useTargetsDownloadManager.ts
+++ hooks/useTargetsDownloadManager.ts
@@ -4,17 +4,17 @@ import { addTargets, updateHotspots } from "lib/firebase";
 import { useTrip } from "providers/trip";
 import { useWindowActive } from "hooks/useWindowActive";
 
-const CUTOFF = "5%";
+const CUTOFF = "5"; // Percent
 const RETRY_LIMIT = 1;
 
 export default function useTargetsDownloadManager() {
   const { trip } = useTrip();
   const [downloadingLocId, setDownloadingLocId] = useState<string | null>(null);
-  const isDownloading = !!downloadingLocId;
 
   const processedHotspotsRef = useRef(new Set<string>());
   const pendingHotspotsRef = useRef(new Set<string>());
   const failedAttemptsRef = useRef(new Map<string, number>());
+  const downloadingRef = useRef(false);
 
   const windowIsFocused = useWindowActive({
     onFocus: () => {
@@ -30,27 +30,40 @@ export default function useTargetsDownloadManager() {
     failedAttemptsRef.current.set(locId, 0);
   };
 
-  useEffect(() => {
-    if (!trip?.hotspots) return;
+  const fetchTargetsForHotspot = async (locId: string): Promise<Targets> => {
+    const url = `https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-6c6abe6c-b02b-4b79-a86e-f7633e99a025/targets/get?startMonth=${trip?.startMonth}&endMonth=${trip?.endMonth}&region=${locId}&cutoff=${CUTOFF}`;
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(response.statusText);
+    const data = await response.json();
+    if (!data.items?.length) throw new Error("No targets found");
+    return data;
+  };
 
-    trip.hotspots
-      .filter((it) => !it.targetsId && !processedHotspotsRef.current.has(it.id))
-      .forEach((it) => pendingHotspotsRef.current.add(it.id));
-
-    if (!isDownloading && !isPaused) downloadPendingTargets();
-  }, [trip, isPaused, isDownloading]);
+  const handleAddTargets = async (locId: string, data: Targets) => {
+    if (!trip) return;
+    const targetsId = await addTargets({ ...data, tripId: trip?.id, hotspotId: locId });
+    if (targetsId && locId) {
+      const newHotspots = trip.hotspots.map((it) => {
+        if (it.id === locId) return { ...it, targetsId };
+        return it;
+      });
+      await updateHotspots(trip.id, newHotspots);
+    }
+  };
 
   const downloadPendingTargets = async () => {
+    if (downloadingRef.current || isPaused) return;
+    downloadingRef.current = true;
+
     while (pendingHotspotsRef.current.size > 0) {
       const locId = pendingHotspotsRef.current.values().next().value;
       if (!locId) break;
-      setDownloadingLocId(locId);
 
+      setDownloadingLocId(locId);
       pendingHotspotsRef.current.delete(locId);
 
       try {
         const targets: Targets = await fetchTargetsForHotspot(locId);
-
         if (targets) {
           await handleAddTargets(locId, targets);
           processedHotspotsRef.current.add(locId);
@@ -67,28 +80,22 @@ export default function useTargetsDownloadManager() {
     }
 
     setDownloadingLocId(null);
+    downloadingRef.current = false;
   };
 
-  const handleAddTargets = async (locId: string, data: Targets) => {
-    const targetsId = await addTargets(data);
-    if (targetsId && locId) {
-      if (!trip) return;
-      const newHotspots = trip.hotspots.map((it) => {
-        if (it.id === locId) return { ...it, targetsId };
-        return it;
-      });
-      await updateHotspots(trip.id, newHotspots);
+  useEffect(() => {
+    if (!trip?.hotspots) return;
+
+    trip.hotspots
+      .filter(
+        (it) => !it.targetsId && !processedHotspotsRef.current.has(it.id) && !pendingHotspotsRef.current.has(it.id)
+      )
+      .forEach((it) => pendingHotspotsRef.current.add(it.id));
+
+    if (!downloadingRef.current && !isPaused) {
+      downloadPendingTargets();
     }
-  };
-
-  const fetchTargetsForHotspot = async (locId: string): Promise<Targets> => {
-    const url = `https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-6c6abe6c-b02b-4b79-a86e-f7633e99a025/targets/get?startMonth=${trip?.startMonth}&endMonth=${trip?.endMonth}&region=${locId}&cutoff=${CUTOFF}`;
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(response.statusText);
-    const data = await response.json();
-    if (!data.items?.length) throw new Error("No targets found");
-    return data;
-  };
+  }, [trip?.hotspots, isPaused]);
 
   const pendingLocIds = Array.from(pendingHotspotsRef.current);
   if (downloadingLocId) pendingLocIds.push(downloadingLocId);
@@ -97,5 +104,5 @@ export default function useTargetsDownloadManager() {
     .filter(([_, attempts]) => attempts >= RETRY_LIMIT)
     .map(([locId]) => locId);
 
-  return { pendingLocIds, failedLocIds, downloadingLocId, isDownloading, retryDownload };
+  return { pendingLocIds, failedLocIds, downloadingLocId, retryDownload };
 }
