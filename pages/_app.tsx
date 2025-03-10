e289de71cbad531f6f8537b10aea7bf547e4a03f
Adam Jackson <akjackson1@gmail.com>
2025-03-10 09:37:03 -0700

    Implement storage persistence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,9 @@ import { QueryClient, QueryClientProvider, QueryCache } from "@tanstack/react-qu
 import { get } from "lib/helpers";
 import { toast } from "react-hot-toast";
 import ErrorBoundary from "components/ErrorBoundary";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { useEffect } from "react";
 
 let queryClient: QueryClient | undefined;
 
@@ -22,7 +25,7 @@ export function initQueryClient() {
     defaultOptions: {
       queries: {
         retry: 1,
-        gcTime: 30 * 24 * 60 * 60 * 1000,
+        gcTime: 7 * 24 * 60 * 60 * 1000, // 7 days
         staleTime: 0,
         queryFn: async ({ queryKey, meta }) =>
           get(queryKey[0] as string, (queryKey[1] || {}) as any, !!meta?.showLoading),
@@ -41,6 +44,21 @@ export function initQueryClient() {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const localStoragePersister = createSyncStoragePersister({
+        storage: window.localStorage,
+        key: "BIRDPLAN_QUERY_CACHE",
+      });
+
+      persistQueryClient({
+        queryClient: queryClient!,
+        persister: localStoragePersister,
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      });
+    }
+  }, []);
+
   return (
     <ErrorBoundary>
       <QueryClientProvider client={initQueryClient()}>
