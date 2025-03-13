7345343261769d54a90030a64ea0bb532fd7aa08
Adam Jackson <akjackson1@gmail.com>
2025-03-12 17:50:49 -0700

    Update useFetchRecentChecklists.ts

--- hooks/useFetchRecentChecklists.ts
+++ hooks/useFetchRecentChecklists.ts
@@ -6,7 +6,8 @@ import { EBIRD_BASE_URL } from "lib/config";
 
 export default function useFetchRecentChecklists(region?: string) {
   const { data, isLoading, error } = useQuery<RecentChecklist[]>({
-    queryKey: [`${EBIRD_BASE_URL}/product/lists/${region}`, { maxResults: 40, key: process.env.NEXT_PUBLIC_EBIRD_KEY }],
+    // Proxy required for this particular eBird endpoint to avoid CORS error
+    queryKey: [`/api/ebird-proxy/product/lists/${region}`, { maxResults: 40, key: process.env.NEXT_PUBLIC_EBIRD_KEY }],
     enabled: !!region,
     staleTime: 30 * 60 * 1000, // 30 minutes
     gcTime: 60 * 60 * 1000, // 60 minutes
