4c95e20f716da2d67ae16173eb14b31ed7b0808c
Adam Jackson <akjackson1@gmail.com>
2025-04-10 15:12:13 -0700

    Refactor reset-password route

--- app/api/v1/reset-password/route.ts
+++ app/api/v1/reset-password/route.ts
@@ -1,33 +1,34 @@
 import { connect, Profile } from "lib/db";
 import { auth } from "lib/firebaseAdmin";
 import { APIError } from "lib/api";
+import dayjs from "dayjs";
 
 export async function POST(request: Request) {
   try {
-    const { token, email, password } = await request.json();
+    const { token, password } = await request.json();
 
-    if (!token || !email || !password) {
+    if (!token || !password) {
       return APIError("Missing required fields", 400);
     }
 
     await connect();
 
-    const user = await auth.getUserByEmail(email);
+    const profile = await Profile.findOne({ resetToken: token }).lean();
 
-    if (user.providerData.some((provider) => provider.providerId === "google.com")) {
-      return APIError("This account uses Google to sign in. Please use the 'Sign in with Google' option.", 400);
-    } else if (user.providerData.some((provider) => provider.providerId === "apple.com")) {
-      return APIError("This account uses Apple to sign in. Please use the 'Sign in with Apple' option.", 400);
-    }
-
-    const profile = await Profile.findOne({ uid: user.uid }).lean();
-
-    if (!profile || profile.resetToken !== token) {
+    if (!profile || !profile.uid) {
       return APIError("Invalid or expired token", 400);
     }
 
-    if (!profile.resetTokenExpires || new Date() > new Date(profile.resetTokenExpires)) {
-      return APIError("Reset token has expired. Please request a new password reset.", 400);
+    const user = await auth.getUser(profile.uid);
+
+    if (user.providerData.some((provider) => provider.providerId === "google.com")) {
+      return APIError("You must use 'Sign in with Google' to login", 400);
+    } else if (user.providerData.some((provider) => provider.providerId === "apple.com")) {
+      return APIError("You must use 'Sign in with Apple' to login", 400);
+    }
+
+    if (!profile.resetTokenExpires || dayjs().isAfter(dayjs(profile.resetTokenExpires))) {
+      return APIError("Reset token has expired", 400);
     }
 
     await auth.updateUser(user.uid, { password });
