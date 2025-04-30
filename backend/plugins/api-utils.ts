3b41ea8a2a6117ef7afb3dc118da9fd4ff29ea46
Adam Jackson <akjackson1@gmail.com>
2025-04-30 16:38:04 -0700

    FIx firebase config

--- backend/plugins/api-utils.ts
+++ backend/plugins/api-utils.ts
@@ -1,41 +1,42 @@
 import { FastifyInstance, FastifyPluginAsync, FastifyRequest } from "fastify";
 import fp from "fastify-plugin";
-import * as admin from "firebase-admin";
-
-/*let firebaseApp: admin.app.App;
-if (!admin.apps.length) {
-  firebaseApp = admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: "bird-planner",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    } as admin.ServiceAccount),
-    storageBucket: "bird-planner.appspot.com",
-  });
-} else {
-  firebaseApp = admin.app();
-}
-
-async function authenticate(request: FastifyRequest): Promise<admin.auth.DecodedIdToken | null> {
-  const authHeader = request.headers.authorization;
-
-  if (!authHeader?.startsWith("Bearer ")) {
-    request.log.error("Missing or invalid authorization header");
-    return null;
-  }
-
-  const token = authHeader.split("Bearer ")[1];
-
-  try {
-    return await firebaseApp.auth().verifyIdToken(token);
-  } catch (error) {
-    request.log.error({ error }, "Firebase auth error");
-    return null;
-  }
-}*/
+import admin from "firebase-admin";
 
 const apiUtils: FastifyPluginAsync = async (fastify: FastifyInstance): Promise<void> => {
-  //fastify.decorate("authenticate", authenticate);
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: "bird-planner",
+        privateKey: fastify.config.FIREBASE_PRIVATE_KEY,
+        clientEmail: fastify.config.FIREBASE_CLIENT_EMAIL,
+      }),
+      storageBucket: "bird-planner.appspot.com",
+    });
+  }
+
+  const auth = admin.auth();
+
+  async function authenticate(request: FastifyRequest): Promise<admin.auth.DecodedIdToken | null> {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader?.startsWith("Bearer ")) {
+      request.log.error("Missing or invalid authorization header");
+      return null;
+    }
+
+    const token = authHeader.split("Bearer ")[1];
+
+    try {
+      return await auth.verifyIdToken(token);
+    } catch (error) {
+      request.log.error({ error }, "Firebase auth error");
+      return null;
+    }
+  }
+
+  fastify.decorate("authenticate", authenticate);
+  fastify.decorate("firebase", admin);
+  fastify.decorate("firebaseAuth", auth);
 };
 
 export default fp(apiUtils, {
