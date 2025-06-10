356188c65b645f7f4f9a22b6fec6e82a387d9c4d
Adam Jackson <akjackson1@gmail.com>
2025-06-09 18:57:39 -0700

    Update index.ts

--- backend/src/trips/index.ts
+++ backend/src/trips/index.ts
@@ -2,22 +2,17 @@ import { Hono } from "hono";
 import trip from "./[id]/index.js";
 import { authenticate } from "lib/utils.js";
 import { connect, Trip } from "lib/db.js";
-import { HTTPException } from "hono/http-exception";
 
 const trips = new Hono();
 
 trips.route("/:id", trip);
 
 trips.get("/", async (c) => {
-  try {
-    const session = await authenticate(c);
+  const session = await authenticate(c);
 
-    await connect();
-    const trips = await Trip.find({ userIds: session.uid }).sort({ createdAt: -1 }).lean();
-    return c.json(trips);
-  } catch (error: unknown) {
-    throw new HTTPException(500, { message: "Error loading trips" });
-  }
+  await connect();
+  const trips = await Trip.find({ userIds: session.uid }).sort({ createdAt: -1 }).lean();
+  return c.json(trips);
 });
 
 export default trips;
