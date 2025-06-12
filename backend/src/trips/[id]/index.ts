66a0ea6a0fbc0fc97d650e4220118208f0fa5aa9
Adam Jackson <akjackson1@gmail.com>
2025-06-11 18:50:48 -0700

    Add export route

--- backend/src/trips/[id]/index.ts
+++ backend/src/trips/[id]/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
-import { authenticate } from "lib/utils.js";
+import { authenticate, tripToGeoJson, sanitizeFileName } from "lib/utils.js";
 import { connect, Trip, TargetList, Invite, Profile } from "lib/db.js";
 import type { TripUpdateInput, Editor } from "shared/types.js";
 import { TargetListType } from "shared/enums.js";
+// @ts-ignore
+import * as tokml from "@maphubs/tokml";
 
 const trip = new Hono();
 
@@ -143,6 +145,43 @@ trip.get("/editors", async (c) => {
   }));
 
   return c.json(editors);
+});
+
+trip.get("/export", async (c) => {
+  const id: string | undefined = c.req.param("id");
+  const uid: string | undefined = c.req.query("uid");
+
+  if (!id) {
+    throw new HTTPException(400, { message: "Trip ID is required" });
+  }
+
+  await connect();
+  const [trip, hotspotTargets, profile] = await Promise.all([
+    Trip.findById(id).lean(),
+    TargetList.find({ tripId: id, type: TargetListType.hotspot }).lean(),
+    Profile.findOne({ uid }).lean(),
+  ]);
+
+  if (!trip) {
+    throw new HTTPException(404, { message: "Trip not found" });
+  }
+
+  const lifelist = profile?.lifelist || [];
+
+  const filteredTargets = hotspotTargets.map((target) => {
+    const needs = target.items?.filter((it) => !lifelist?.includes(it.code));
+    const filtered = needs?.filter((it) => it.percentYr >= 5);
+    const items = filtered?.sort((a, b) => b.percentYr - a.percentYr);
+    return { ...target, items };
+  });
+
+  const geoJson = tripToGeoJson(trip, filteredTargets);
+  const kml = tokml(geoJson);
+
+  return c.body(kml, 200, {
+    "Content-Type": "application/vnd.google-earth.kml+xml",
+    "Content-Disposition": `attachment; filename="${sanitizeFileName(trip.name)}.kml"`,
+  });
 });
 
 export default trip;
