837db1cee1168a673ac0953c49e3e85d84f5cf93
Adam Jackson <akjackson1@gmail.com>
2025-06-17 17:47:31 -0700

    API route bug fixes

--- frontend/pages/[tripId]/quiz.tsx
+++ frontend/pages/[tripId]/quiz.tsx
@@ -43,7 +43,7 @@ export default function Quiz() {
 
     const randomCodes = getRandomItemsFromArray(targetCodes, quizLength);
     try {
-      const res = await fetch(`/quiz/generate`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/quiz/generate`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ codes: randomCodes }),
