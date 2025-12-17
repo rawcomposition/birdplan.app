3c498b00f62fa4e257cd25e6a411d3eb9bde779c
Adam Jackson <akjackson1@gmail.com>
2025-12-17 09:31:30 -0800

    Update whats-new.tsx

--- frontend/pages/whats-new.tsx
+++ frontend/pages/whats-new.tsx
@@ -15,6 +15,10 @@ export default function WhatsNew() {
       <main className="container px-4">
         <h1 className="text-4xl text-gray-800 leading-normal font-bold mt-12">What&apos;s New</h1>
         <div className="prose prose-h2:text-gray-800 prose-h2:mt-12 prose-ul:list-none prose-ul:pl-4 mb-12">
+          <h2>December 17, 2025</h2>
+          <ul>
+            <li>✨ Updated to the latest version of Avicommons.org data for species thumbnails.</li>
+          </ul>
           <h2>November 4, 2025</h2>
           <ul>
             <li>
