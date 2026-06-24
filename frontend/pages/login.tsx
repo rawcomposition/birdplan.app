e592effc436d4377670f7eaf8a34fed5232d2fcc
Adam Jackson <akjackson1@gmail.com>
2026-06-24 09:02:02 -0700

    And notices

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UtilityPage from "components/UtilityPage";
 import AuthForm from "components/AuthForm";
+import Alert from "components/Alert";
 import { Navigate, useSearchParams } from "react-router-dom";
 import { useUser } from "hooks/useUser";
 import { getPostAuthDest } from "lib/helpers";
@@ -22,6 +23,12 @@ export default function Login() {
 
   return (
     <UtilityPage heading="Welcome">
+      <Alert style="info" className="mb-4">
+        <p>
+          <strong>We’ve simplified sign-in.</strong> Google sign-in is no longer available, but you can still access
+          your account by entering your email below.
+        </p>
+      </Alert>
       <AuthForm heading="Enter your email" message={message} email={email} />
     </UtilityPage>
   );
