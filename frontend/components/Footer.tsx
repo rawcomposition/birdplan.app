87ccf25ac1847a45ede9903a9a2dd75fa1fcb9c0
Adam Jackson <akjackson1@gmail.com>
2026-06-27 10:09:17 -0700

    Show footer on mobile

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto text-center text-[12px] opacity-80 text-gray-500 border-t py-4 hidden md:block">
+    <footer className="mt-auto text-center text-[12px] opacity-80 text-gray-500 border-t py-4">
       <Link to="/contact">Contact</Link>
       <span className="mx-2">•</span>
       <Link to="/whats-new">What&apos;s New</Link>
